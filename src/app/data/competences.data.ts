53c8e8f76537890c9ee472676c4eb8a4df5c17f6
nal95 <alex.nguepi@yahoo.fr>
2023-08-02 22:26:50 +0200

    update data
    Signed-off-by: nal95 <alex.nguepi@yahoo.fr>

--- src/app/data/competences.data.ts
+++ src/app/data/competences.data.ts
@@ -23,38 +23,38 @@ export const MyData: Competences = {
   id: 1,
   data: {
     language: [
-      {id:1, name:"C/C++",level:3},{id:2, name:"Java",level:4},{id:3, name:"Javascript",level:4},{id:4, name:"Python",level:2},
-      {id:5, name:"Ruby",level:3},{id:6, name:"Shell",level:2},{id:7, name:"SQL",level:4},{id:8, name:"Solidity",level:3},
-      {id:9, name:"Typescript",level:4},{id:10, name:"VHDL",level:2},
+      {id:1, name:"Java", level:4},{id:2, name:"JavaScript", level:4},{id:3, name:"TypeScript", level:4},{id:4, name:"SQL", level:4},
+      {id:5, name:"C/C++", level:3},{id:6, name:"Ruby", level:3},{id:7, name:"Solidity", level:3},{id:8, name:"Python", level:2},
+      {id:9, name:"Shell", level:2},{id:10, name:"VHDL", level:2},
     ],
     framework: [
-      {id:1, name:"Angular",level:4},{id:2, name:"Qt",level:2},{id:3, name:"Quarkus",level:3},{id:4, name:"Node js",level:3},
-      {id:5, name:"React",level:2}, {id:6, name:"Ruy on Rails",level:3},{id:7, name:"Spring Boot",level:4}, {id:8, name:"Vue Js",level:1},
-      {id:9, name:".NET",level:2}
+      {id:1, name:"Angular", level:4},{id:2, name:"Spring Boot", level:4},{id:3, name:"Quarkus", level:3},{id:4, name:"Node js", level:3},
+      {id:5, name:"Ruy on Rails", level:3},{id:6, name:"React", level:2},{id:7, name:"Qt", level:2},{id:8, name:".NET", level:2},
+      {id:9, name:"Vue Js", level:1},
     ],
     cloud: [
-      {id:1, name:"AWS",level:2},{id:2, name:"Azure",level:1},{id:3, name:"Google Cloud",level:3},{id:4, name:"Lenode",level:2},
-      {id:5, name:"Oracle",level:1}
+      {id:1, name:"Google Cloud", level:3},{id:2, name:"AWS", level:2},{id:3, name:"Lenode", level:2},{id:4, name:"Azure", level:2},
+      {id:5, name:"Oracle", level:1}
     ],
     devops: [
-      {id:1, name:"Docker",level:3},{id:2, name:"Github Action ",level:3},{id:3, name:"Helm ",level:2},{id:4, name:"Jenkins",level:3},
-      {id:5, name:"Kubernetes",level:3}, {id:6, name:"Terraform",level:3}
+      {id:1, name:"Docker", level:3},{id:2, name:"Github Action ", level:3},{id:3, name:"Jenkins", level:3},{id:4, name:"Kubernetes", level:3},
+      {id:5, name:"Terraform", level:3},{id:6, name:"Helm ", level:2},
     ],
     database: [
-      {id:1, name:"BigQuery",level:3},{id:2, name:"Mysql",level:4},{id:3, name:"Monogo DB",level:3},{id:4, name:"Postgres Sql",level:4},
+      {id:1, name:"Mysql", level:4},{id:2, name:"Postgres Sql", level:4},{id:3, name:"Monogo DB", level:3},{id:4, name:"BigQuery", level:3},
     ],
     testing: [
-      {id:1, name:"Cypress",level:3},{id:2, name:"JUnit",level:3},{id:3, name:"Jest",level:3},{id:3, name:"Karma",level:3}
+      {id:1, name:"Cypress", level:3},{id:2, name:"JUnit", level:3},{id:3, name:"Jest", level:3},{id:3, name:"Karma", level:3}
     ],
     os: [
-      {id:1, name:"Unix / Linux",level:3},{id:2, name:"Windows",level:5}
+      {id:2, name:"Windows", level:5},{id:1, name:"Unix / Linux", level:3}
     ],
     ide: [
-      {id:1, name:"Intellij",level:5},{id:2, name:"DB Visualizer",level:2},{id:3, name:"Visual Studio",level:4},{id:4, name:"VS Code",level:5}
+      {id:1, name:"Intellij", level:5},{id:2, name:"VS Code", level:5},{id:3, name:"Visual Studio", level:4},{id:4, name:"DB Visualizer", level:2}
     ],
     tools: [
-      {id:1, name:"Ethereum",level:3},{id:2, name:"Git",level:4},{id:3, name:"Polygon",level:3},{id:4, name:"Raspberry pi",level:3},
-      {id:5, name:"Microsoft Office",level:4}
+      {id:1, name:"Microsoft Office", level:4},{id:2, name:"Ethereum", level:3},{id:3, name:"Metamask", level:3},{id:4, name:"Git", level:4},{id:5, name:"Polygon", level:3},
+      {id:6, name:"Raspberry pi", level:3},{id:7, name:"Docker Hub", level:3}
     ],
     certifications:[]
   }
--- src/app/data/enterprise.data.ts
+++ src/app/data/enterprise.data.ts
@@ -8,4 +8,39 @@ export const enterprise : Enterprise [] = [
   {uuid: '1', name: "Adesso"},
   {uuid: '2', name: "Hiqs"},
   {uuid: '3', name: "Hochschulstart"},
+  {uuid: '4', name: "Valantic"},
+  {uuid: '5', name: "Nesto Software"},
+  {uuid: '6', name: "MaibornWolff GmbH"},
+  {uuid: '7', name: "sifamo GmbH"},
+  {uuid: '8', name: "AEMtec GmbH"},
+  {uuid: '9', name: "S Rating und Risikosysteme GmbH"},
+  {uuid: '10', name:"Collaboration Betters The World GmbH"},
+  {uuid: '11', name:"Amadeus FiRe AG"},
+  {uuid: '12', name:"Astriol academics GmbH"},
+  {uuid: '13', name:"USU Software AG"},
+  {uuid: '14', name:"ConVista Faktor Zehn GmbH"},
+  {uuid: '15', name:"Pertemps ERP"},
+  {uuid: '16', name:"init AG"},
+  {uuid: '17', name:"Capgemini"},
+  {uuid: '18', name:"RODIAS"},
+  {uuid: '19', name:"BOSCH"},
+  {uuid: '20', name:"Hypoport Karriere"},
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
