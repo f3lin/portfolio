bbabdff00d4b9210779adf06ac9bac2bf3b97c14
nal95 <alex.nguepi@yahoo.fr>
2023-08-02 07:31:18 +0200

    add banner to hiqs
    Signed-off-by: nal95 <alex.nguepi@yahoo.fr>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./portfolio/home/home.component";
-import {AboutComponent} from "./portfolio/about/about.component";
 import {ExperienceComponent} from "./portfolio/experience/experience.component";
 import {ContactComponent} from "./portfolio/contact/contact.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home/:id', component: HomeComponent }, //TODO: exploiter cette route
-  { path: 'about', component: AboutComponent },
+  { path: 'home/:id', component: HomeComponent },
   { path: 'experience', component: ExperienceComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }
