9a7e64d121ecdccc440a39ac0c281c840be3cd19
nal95 <alex.nguepi@yahoo.fr>
2023-08-02 10:31:21 +0200

    add data, add certifications tap
    Signed-off-by: nal95 <alex.nguepi@yahoo.fr>

--- src/app/data/competences.data.ts
+++ src/app/data/competences.data.ts
@@ -16,37 +16,46 @@ export interface Competences {
     os: Corpus[];
     ide: Corpus[];
     tools: Corpus[];
+    certifications: Corpus [];
   };
 }
 export const MyData: Competences = {
   id: 1,
   data: {
     language: [
-      {id:1, name:"c++",level:2},{id:2, name:"java",level:3},{id:3, name:"javascript",level:4},{id:4, name:"solidity",level:5}
+      {id:1, name:"C/C++",level:3},{id:2, name:"Java",level:4},{id:3, name:"Javascript",level:4},{id:4, name:"Python",level:3},
+      {id:5, name:"Ruby",level:3},{id:5, name:"SQL",level:4},{id:6, name:"Typescript",level:4},{id:7, name:"Solidity",level:4},
+      {id:8, name:"Shell",level:4},{id:9, name:"VHDL",level:2},
     ],
     framework: [
-      {id:1, name:"Springboot",level:2},{id:2, name:"angular",level:3},{id:3, name:"react",level:4},{id:4, name:"quarkus",level:5}
+      {id:1, name:"Angular",level:4},{id:2, name:"Qt",level:2},{id:3, name:"Quarkus",level:3},{id:4, name:"Node js",level:3},
+      {id:5, name:"React",level:2}, {id:6, name:"Ruy on Rails",level:3},{id:7, name:"Springboot",level:4}, {id:8, name:"Vue Js",level:1},
+      {id:9, name:".NET",level:2}
     ],
     cloud: [
-      {id:1, name:"Azure",level:2},{id:2, name:"Google Cloud",level:3},{id:3, name:"Oracle",level:5}
+      {id:1, name:"AWS",level:2},{id:2, name:"Azure",level:1},{id:3, name:"Google Cloud",level:3},{id:4, name:"Lenode",level:2},
+      {id:5, name:"Oracle",level:1}
     ],
     devops: [
-      {id:1, name:"Jenkins",level:2},{id:2, name:"Github Action",level:3},{id:3, name:"Terraform",level:4},{id:4, name:"Kubernetes",level:5},{id:5, name:"Helm",level:6},{id:5, name:"Docker",level:3}
+      {id:1, name:"Docker",level:3},{id:2, name:"Github Action ",level:3},{id:3, name:"Helm ",level:2},{id:4, name:"Jenkins",level:3},
+      {id:5, name:"Kubernetes",level:3}, {id:6, name:"Terraform",level:3}
     ],
     database: [
-      {id:1, name:"Mysql",level:2},{id:2, name:"Postgres Sql",level:3},{id:3, name:"Monogo DB",level:4}
+      {id:1, name:"BigQuery",level:3},{id:1, name:"Mysql",level:4},{id:2, name:"Monogo DB",level:3},{id:3, name:"Postgres Sql",level:4},
     ],
     testing: [
-      {id:1, name:"JUnit",level:2},{id:2, name:"Jest",level:3},{id:3, name:"Karma",level:4},{id:4, name:"Cypress",level:5}
+      {id:1, name:"Cypress",level:3},{id:2, name:"JUnit",level:3},{id:3, name:"Jest",level:3},{id:3, name:"Karma",level:3}
     ],
     os: [
-      {id:1, name:"Unix / Linux",level:4},{id:2, name:"Windows",level:5}
+      {id:1, name:"Unix / Linux",level:3},{id:2, name:"Windows",level:5}
     ],
     ide: [
-      {id:1, name:"VS Code",level:5},{id:2, name:"Intellij",level:5},{id:3, name:"Visual Studio",level:4},{id:4, name:"DB Visualizer",level:2}
+      {id:1, name:"Intellij",level:5},{id:2, name:"DB Visualizer",level:2},{id:3, name:"Visual Studio",level:4},{id:4, name:"VS Code",level:5}
     ],
     tools: [
-      {id:1, name:"Raspberry pi",level:3},{id:2, name:"Git",level:4},{id:3, name:"Microsoft Office",level:4}
-    ]
+      {id:1, name:"Ethereum",level:3},{id:2, name:"Git",level:4},{id:3, name:"Polygon",level:3},{id:4, name:"Raspberry pi",level:3},
+      {id:5, name:"Microsoft Office",level:4}
+    ],
+    certifications:[]
   }
 }
